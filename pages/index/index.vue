2195b1625765f797dab0b300ce625577ee146080
Eve-Middleton <2319048739@qq.com>
2022-08-01 23:41:16 +0800

    完善canvas绘制，大致轮廓已完成

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,7 +1,7 @@
 <template>
 	<view>
 		<button @click="ToImg">生成图片</button>
-		<canvas style="width: 500px; height: 300px;" canvas-id="firstCanvas" id="firstCanvas"></canvas>
+		<canvas style="width: 500px;height: 500px;" canvas-id="firstCanvas" id="firstCanvas"></canvas>
 		<!-- <canvas style="width: 400px; height: 500px;" canvas-id="secondCanvas" id="secondCanvas"></canvas>
 		<canvas style="width: 400px; height: 500px;" canvas-id="secondCanvas" id="secondCanvas" @error="canvasIdErrorCallback"></canvas> -->
 	</view>
@@ -15,26 +15,72 @@ export default {
 	}, 
 	created()  {
 		var context = uni.createCanvasContext('firstCanvas')
-			context.drawImage('http://pic.bizhi360.com/bbpic/51/10551.jpg')
-							
+			//context.drawImage('http://pic.bizhi360.com/bbpic/51/10551.jpg')
+						
 		//背景填充
-		// this.fillRoundRect(context, 0, 0, 500, 300, 0, 'rgba(44, 225, 12, 0.7)');
+		 this.fillRoundRect(context, 0, 0, 500, 500, 0, 'rgba(93, 93, 93, 0.7)');
 		//时间框绘制
-		this.strokeRoundRect(context, 130, 10, 60,20, 5,1,"#8b8b8b");
+		this.strokeRoundRect(context, 195, 42, 110,20, 5,1,"#8b0002");
+		//绘制时间
+		// context.font="Microsoft Yahei";
+		 context.font="13px Microsoft Yahei";
+		context.fillText("2022-8-1 10:00",200,58);
+		
+		//类型绘制
+		 context.font="15px Microsoft Yahei";
+		this.strokeRoundRect(context,20,80,50,20,5,1,"#8b0002");
+		//绘制类别文字
+		context.fillText("提问",30,95);
+		
+		//投稿内容
+		var txt="小黑板你好小黑板你好小黑板你好小黑板你好小黑板你好小黑板你好小黑板你好小黑板你好";
+		//var txt="小黑板你好";
+		let result = this.text(txt)
 		//投稿气泡绘制
-		this.strokeRoundRect(context, 50, 50, 300,100, 5,1,"#8b8b8b");
+		//如果文字占一行，则根据文字长短调整气泡x轴
+		let resWitch=389;
+		if(result.rows==1){
+			resWitch=result.strLength*result.fontSize+30;
+			
+		}
+		//根据文字行数确定气泡的高 35为单行高度
+		this.strokeRoundRect(context, 20, 105, resWitch,result.rows*35, 10,1,"#8b0002");
+		//绘制投稿内容
+		 context.font="18px Microsoft Yahei";
+		for (let i = 0; i <= result.rows; i++) {
+		//35为文字x方向起始位置，115为文字y轴起始方向，23为每增加一行，y轴下移23px
+		 context.fillText(txt.slice(result.rowFontNum*(i-1),result.rowFontNum*i),35,110+i*23)
+		}
+		context.save();
 		//小黑板气泡绘制
-		this.strokeRoundRect(context, 300, 200, 50,50, 5,1,"#8b8b8b");
+		this.strokeRoundRect(context, 322, 301, 100,50, 10,1,"#8b0002");
+		context.fillText("板板收到~",325,330);
+		//小黑板头像绘制
+		this.drawCircle(context,463,302,25,-(1 / 2 * Math.PI),-(1 / 2 * Math.PI),1);
 		context.draw();
 	},
 	methods: {
+		//canvas绘制文字换行
+		text(str) {
+		      // 画布总宽度 px单位
+		      let canvasWidth = 360
+		      // 字体大小 px单位
+		      let fontSize = 18
+		      // 每行所需字数 = 画布总宽度 / 单个字体大小
+		      let rowFontNum = Math.floor(canvasWidth / fontSize)
+		      // 字符串总长度
+		      let strLength = str.length
+		      // 所需行数 = 字符总长度 / 每行所需字数
+		      let rows = Math.ceil(strLength / rowFontNum)
+		      return {canvasWidth,fontSize,rowFontNum,strLength,rows}
+		  },
 		ToImg(){
 			var urls=[];
 			uni.canvasToTempFilePath({
 			  // x: 100,起始点
 			  // y: 200,结束点
 			 width:500,
-			 height:300,
+			 height:500,
 			 destWidth: 1000,
 			   destHeight: 1000,
 			  canvasId: 'firstCanvas',
@@ -58,19 +104,13 @@ export default {
 			  } 
 			})
 			
-			
-					// uni.saveImageToPhotosAlbum({
-					// 	 filePath: this.url,
-					// 	// filePath: res.tempFilePaths[0],
-					// 	success: function () {
-					// 		console.log('save success');
-					// 	}
-					// });
-			
 		},
-		starp(){		
-		//绘制并填充一个圆角矩形  
-		this.fillRoundRect(context, 200, 10, 100, 50, 10, 'rgba(0,0,0,0.7)');
+		drawCircle(cxt,x,y,cR,startAngle,tmpAngle,lineWidth){
+			 cxt.beginPath();
+			    cxt.lineWidth = lineWidth;
+			    cxt.strokeStyle = '#8b0002';
+			    cxt.arc(x, y, cR, startAngle, tmpAngle);
+			    cxt.stroke();
 		},
 		/**该方法用来绘制一个有填充色的圆角矩形 
 		     *@param cxt:canvas的上下文环境 
