e0a638370b7f7d98a67484525f6d5318f5cd1766
Eve-Middleton <2319048739@qq.com>
2022-08-08 16:10:01 +0800

    完善稿件分段分条绘制

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -21,7 +21,8 @@ export default {
 				frameTopLeftY:0,
 				frameWidth:500,
 				frameHeight:280,
-				xhbImgSrc:"../../static/bjt.jpg"
+				xhbImgSrc:"../../static/xhbbjt.jpg",
+				bbbImgSrc:"../../static/bbbbjt.jpg",
 			},
 				//时间
 				timeData:{
@@ -46,9 +47,10 @@ export default {
 					computeDrawHeight:'',
 					contentTopLeftX:33,
 					contentTopLeftY:117,
-					txt:'',
+					//投稿内容空行分割结果
+					txt:[],
 					fontSizeAndStyle:"20px Microsoft Yahei",
-					result:{},
+					results:[],
 					radius:10,//圆角
 					fontColor:"#333333",
 					fillColor:'rgba(230,230,230,0.7)'
@@ -70,20 +72,25 @@ export default {
 				},
 				//回复气泡
 				respondData:{
-					frameTopLeftX:'',
-					frameTopLeftY:'',
-					frameWidth:'',
-					frameHeight:'',
-					contentTopLeftX:'',
-					contentTopLeftY:'',
+					frameTopLeftX:'322',
+					frameTopLeftY:'270',
+					frameWidth:'110',
+					frameHeight:'60',
+					contentTopLeftX:'327',
+					contentTopLeftY:'305',
+					radius:10 ,//圆角
+					fillColor:'rgb(0,153,255)'
 				},
+				//头像
 				headshotData:{
 					frameCenterX:'',
 					frameCenterY:'',
 					frameR:'',
-					xhbUrl:'',
+					xhbUrl:'../../static/nzxhb.jpg',
 					bbbUrl:''
-				}
+				},
+				//投稿原始内容
+				rsStr:""
 
 			
 		}
@@ -92,84 +99,34 @@ export default {
 		//var context = uni.createCanvasContext('firstCanvas')
 	},
 	created()  {
-		this.submitData.txt="小黑板你好小黑板你好小黑板你好小黑板你好小黑板你好小黑板你好小黑板小黑板你好小黑板你好小黑板你好小黑板你好小黑板你好小小黑板你好小黑板你好小黑板你好小黑板你好小黑板你好小小黑板你好小黑板你好小黑板你好小黑板你好小黑板你好小小黑板你好小黑板你好小黑板你好小黑板你好小黑板你好小你好小黑板你好";
-		//var txt="小黑板你好";
-		this.submitData.result = this.text(this.submitData.txt)
-		this.submitData.DrawHeight=500-30+this.submitData.result.rows*30
+		this.rsStr="板板你好我要投稿文案\n\n落霞与孤鹜齐飞\n这是长句测试略略略略略略略略略略略略略略略略略略略略略略略略略略略略略略略略略略略略略略略略略略略略略\n\n\n\n\n\n1235";
+		//正则表达式处理字符串回车符，处理后为["A","B"]
+		this.submitData.txt=this.rsStr.replace(/^\s*|\s*$/g,"").replace(/\n+/g,"\n").split('\n');
+		//画布默认高度为450，updateHeight为需要增加的高度
+		var updateHeight=0;
+		//遍历字符串数组，得到段落结果集，详见text()函数
+			for (var i = 0; i < this.submitData.txt.length; i++) {
+			//返回分割后字符串行处理结果集
+			this.submitData.results[i] = this.text(this.submitData.txt[i])
+			//得到需要增加的画布高度
+			updateHeight+=this.submitData.results[i].rows*30
+		}
+		console.log("this.submitData.results",this.submitData.results)
+		//修改画布高度
+		this.submitData.DrawHeight=450-30+updateHeight;
+		console.log(this.submitData.DrawHeight)
+		//字符串拼接得到最终画布高度
 		this.firstCanvas.minHeight=this.submitData.DrawHeight+"px"
-		// var context = uni.createCanvasContext('firstCanvas')
-		// uni.getImageInfo({
-		// 	src:"https://s1.328888.xyz/2022/08/02/Oht1K.jpg",
-		// 	success: (image) => {
-		// 		this.popupImg=image.path;
-		// 		console.log("获取到的本地路径",image)
-		// 	}
-		// })
-		// context.drawImage(this.popupImg,0,0,500,280);
-		// //context.drawImage(imgBase64,0,0,500,280);						
-		// //背景填充
-		//  //this.fillRoundRect(context, 0, 0, 500, 500, 0, 'rgba(93, 93, 93, 0.7)');
-		// //时间框绘制
-		// this.strokeRoundRect(context, 195, 42, 110,20, 5,1,"#8b0002");
-		// //绘制时间
-		// // context.font="Microsoft Yahei";
-		//  context.font="13px Microsoft Yahei";
-		// context.fillText("2022-8-1 10:00",200,58);
 		
-		// //类型绘制
-		//  context.font="15px Microsoft Yahei";
-		// this.strokeRoundRect(context,20,80,50,20,5,1,"#8b0002");
-		// //绘制类别文字
-		// context.fillText("提问",30,95);
-		
-		// //投稿内容
-		// var txt="小黑板你好小黑板你好小黑板你好小黑板你好小黑板你好小黑板你好小黑板你好小黑板你好";
-		// //var txt="小黑板你好";
-		// let result = this.text(txt)
-		// //投稿气泡绘制
-		// //如果文字占一行，则根据文字长短调整气泡x轴
-		// let resWitch=389;
-		// if(result.rows==1){
-		// 	resWitch=result.strLength*result.fontSize+30;
-			
-		// }
-		// //根据文字行数确定气泡的高 35为单行高度
-		// this.strokeRoundRect(context, 20, 105, resWitch,result.rows*35, 10,1,"#8b0002");
-		// //绘制投稿内容
-		//  context.font="18px Microsoft Yahei";
-		// for (let i = 0; i <= result.rows; i++) {
-		// //35为文字x方向起始位置，115为文字y轴起始方向，23为每增加一行，y轴下移23px
-		//  context.fillText(txt.slice(result.rowFontNum*(i-1),result.rowFontNum*i),35,110+i*23)
-		// }
-		// context.save();
-		// //小黑板气泡绘制
-		// this.strokeRoundRect(context, 322, 301, 100,50, 10,1,"#8b0002");
-		// context.fillText("板板收到~",325,330);
-		// //小黑板头像绘制
-		// this.drawCircle(context,463,302,25,-(1 / 2 * Math.PI),-(1 / 2 * Math.PI),1);
-		// context.draw();
 	},
 	methods: {
 		BeginDraw(){
 			// uni.showLoading({
 			// 	title: '加载中'
 			// });
+			//得到画布上下文
 			var context = uni.createCanvasContext('firstCanvas')
-			// uni.getImageInfo({
-			// 	src:"https://s1.328888.xyz/2022/08/02/Oht1K.jpg",
-			// 	success: (image) => {
-			// 		pathToBase64(image.path)
-			// 								.then(base64 => {
-			// 									this.popupImgBase64 = base64
-			// 								})
-			// 								.catch(error => {
-			// 								console.error(error)
-			// 								})
-										
-			// 		console.log("获取到的本地路径",image)
-			// 		console.log("base64",this.popupImgBase64)
-			// 	}
-			// })
+			
 			
 			//根据画布高度遍历背景图片
 			let tempBgH=this.backgroundData.frameTopLeftY;
@@ -192,35 +149,64 @@ export default {
 					context.fillStyle=this.categoryData.fontColor;
 					context.fillText(this.categoryData.contentTxt,this.categoryData.contentTopLeftX,this.categoryData.contentTopLeftY);
 					
-					
+					context.save();
+					context.restore();
 					//投稿绘制
+					//设置字体自号
 					context.font="20px Microsoft Yahei";
 					//投稿内容
 					
 					//投稿气泡绘制
 					//如果文字占一行，则根据文字长短调整气泡x轴
-					let resWitch=this.submitData.frameWidth;
-					if(this.submitData.result.rows==1){
-						resWitch=this.submitData.result.strLength*this.submitData.result.fontSize+30;
-						
-					}
-					//根据文字行数确定气泡的高 35为单行高度
+					let resWitchList=[];
 					
-					this.fillRoundRect(context, this.submitData.frameTopLeftX, this.submitData.frameTopLeftY, resWitch,this.submitData.result.rows*30+25, this.submitData.radius,this.submitData.fillColor);
-					//绘制投稿内容
-					 context.font=this.submitData.fontSizeAndStyle;
-					 context.fillStyle=this.submitData.fontColor;
-					for (let i = 0; i <= this.submitData.result.rows; i++) {
-					//35为文字x方向起始位置，115为文字y轴起始方向，23为每增加一行，y轴下移23px
-					 context.fillText(this.submitData.txt.slice(this.submitData.result.rowFontNum*(i-1),this.submitData.result.rowFontNum*i),this.submitData.contentTopLeftX,this.submitData.contentTopLeftY+i*26)
+					//遍历字符串结果集，多次绘制气泡
+					//统计总行数
+					var countRow=0;
+					for (var i = 0; i < this.submitData.txt.length; i++) {
+						
+						console.log("length", this.submitData.txt.length)
+						console.log(i,this.submitData.results[i].rows)
+						
+						//设置气泡的宽度
+						resWitchList[i]=this.submitData.frameWidth
+						//如果文字只占一行，则根据文字长度动态设置宽度
+						if(this.submitData.results[i].rows==1){
+							resWitchList[i]=this.submitData.results[i].strLength*this.submitData.results[i].fontSize+30;
+						}
+						
+						//根据文字行数确定气泡的高 35为单行高度
+						this.fillRoundRect(context, this.submitData.frameTopLeftX, this.submitData.frameTopLeftY, resWitchList[i],this.submitData.results[i].rows*30+25, this.submitData.radius,this.submitData.fillColor);
+						//绘制投稿内容
+						 context.font=this.submitData.fontSizeAndStyle;
+						 context.fillStyle=this.submitData.fontColor;
+						 
+						 //以回车符分割的单条消息按行绘制
+						for (let j = 0; j <= this.submitData.results[i].rows; j++) {
+						//35为文字x方向起始位置，115为文字y轴起始方向，23为每增加一行，y轴下移23px
+						 context.fillText(this.submitData.txt[i].slice(this.submitData.results[i].rowFontNum*(j-1),this.submitData.results[i].rowFontNum*j),this.submitData.contentTopLeftX,this.submitData.contentTopLeftY+j*26)
+						}
+						
+						//一条绘制完后气泡下移
+						//Y轴下移,下移高度为上一条气泡的高度，10为气泡之前的间隙高度
+						this.submitData.frameTopLeftY+=this.submitData.results[i].rows*30+25+10;
+						this.submitData.contentTopLeftY+=this.submitData.results[i].rows*30+25+10;
+						countRow+=this.submitData.results[i].rows;
 					}
+					
+					console.log("countRow",countRow)
 					context.save();
+					context.restore();
 					//小黑板气泡绘制
-					this.fillRoundRect(context, 322, 270+this.submitData.result.rows*30, 110,60, 10,'rgb(0,153,255)');
+					this.fillRoundRect(context, this.respondData.frameTopLeftX, this.respondData.frameTopLeftY+countRow*30, this.respondData.frameWidth,this.respondData.frameHeight, this.respondData.radius,this.respondData.fillColor);
 					context.fillStyle="#FFFFFF";
-					context.fillText("板板收到~",327,305+this.submitData.result.rows*30);
+					context.fillText("板板收到~",327,305+countRow*30);
+					context.save();
+					context.restore();
 					//小黑板头像绘制
-					this.drawCircle(context,463,302+this.submitData.result.rows*30,25,-(1 / 2 * Math.PI),-(1 / 2 * Math.PI),1);
+					//
+					this.drawCircle(context,this.headshotData.xhbUrl,463,302+countRow*30,25,-(1 / 2 * Math.PI),-(1 / 2 * Math.PI),1);
+					context.drawImage()
 					context.draw();
 					
 			setTimeout(function () {
@@ -275,12 +261,13 @@ export default {
 			})
 			
 		},
-		drawCircle(cxt,x,y,cR,startAngle,tmpAngle,lineWidth){
+		drawCircle(cxt,img,x,y,cR,startAngle,tmpAngle,lineWidth){
 			 cxt.beginPath();
 			    cxt.lineWidth = lineWidth;
-			    cxt.strokeStyle = '#8b0002';
+			    cxt.strokeStyle = '#FFFFFF';
 			    cxt.arc(x, y, cR, startAngle, tmpAngle);
 			    cxt.stroke();
+				cxt.drawImage(img, x, y, cR*2, cR*2);
 		},
 		/**该方法用来绘制一个有填充色的圆角矩形 
 		     *@param cxt:canvas的上下文环境 
@@ -317,7 +304,7 @@ export default {
 		     **/
 		strokeRoundRect(cxt, x, y, width, height, radius, /*optional*/ lineWidth, /*optional*/ strokeColor) {
 		      //圆的直径必然要小于矩形的宽高          
-		      if (2 * radius > width || 2 * radius > height) {
+		      if (2 * radius > width || 2 * radius > height) { 
 		        return false;
 		      }
 		
